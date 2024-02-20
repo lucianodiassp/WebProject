47a68a668d4c6132c6304e39579c82819061fa00
Luciano Dias <lucianodiass@gmail.com>
2024-02-19 22:09:17 -0300

    Created Department

---
+++ SalesWebMvc/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departmentsList = new List<Department>();
+            departmentsList.Add(new Department { Id = 1, Name = "Eletronics"});
+            departmentsList.Add(new Department { Id = 2, Name = "Fashion" });
+            return View(departmentsList);
+        }
+    }
+}
