3456e0b663a8c75e804cf5272e5aa304e12731f8
Luciano Dias <lucianodiass@gmail.com>
2024-02-28 09:06:03 -0300

    Simple Create Form

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -16,5 +16,10 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();
         }
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
