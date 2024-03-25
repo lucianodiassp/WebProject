39347faec0ac1a0c339d57ae4ef0b8166e1f9153
Luciano Dias <lucianodiass@gmail.com>
2024-03-25 20:40:04 -0300

    Seller Details and Eager loading.

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -57,5 +57,20 @@ namespace SalesWebMvc.Controllers
             _sellerService.RemoveById(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
